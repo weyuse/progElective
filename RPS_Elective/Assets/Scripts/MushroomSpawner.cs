6f017e8bdfb2c6c6a10cb2b8ccea6058982cc097
weyuse <45233958+weyuse@users.noreply.github.com>
2024-06-07 01:49:40 +0100

    random mushroom spawns
    random mushroom spawns (currently every 20 seconds) for ai to aim for

---
+++ RPS_Elective/Assets/Scripts/MushroomSpawner.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MushroomSpawner : MonoBehaviour
+{
+    public GameObject mushroomPrefab;
+
+    // Interval between spawns in seconds
+    public float spawnInterval = 20.0f; 
+
+    public float arenaSize = 500.0f; 
+    private float timer = 0.0f;
+
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+        // counting till the set time
+        timer += Time.deltaTime;
+
+        // waiting for timer to reach the required amount of seconds
+        if (timer >= spawnInterval)
+        {
+            // reset the timer
+            timer = 0.0f;
+
+            // generate random spawn position within the arena with a 3d vector
+            Vector3 spawnPosition = new Vector3(Random.Range(-arenaSize, arenaSize), 2, Random.Range(-arenaSize, arenaSize)
+            );
+
+            // instantiate the mushroom at the random position quaternion meaning keeping the rotation as the prefab dictates
+            Instantiate(mushroomPrefab, spawnPosition, Quaternion.identity);
+        }
+    }
+}
