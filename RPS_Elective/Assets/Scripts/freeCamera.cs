e3cffb779046a87d9b091e8a0941f6ecefd7021d
weyuse <45233958+weyuse@users.noreply.github.com>
2024-08-26 15:19:34 +0100

    Camera Movement
    added functionality to have the camera rotate left and right on a/d presses

---
+++ RPS_Elective/Assets/Scripts/freeCamera.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+
+public class freeCamera : MonoBehaviour
+{
+    public Transform target; 
+    private float rotationSpeed = 50f; 
+    private float radius = 1000f; 
+    private float verticalOffset = 400f;
+    private float currentAngle = 0f;
+
+    void Start()
+    {
+        UpdateCameraPosition();
+    }
+
+    void Update()
+    {
+        HandleRotation();
+    }
+
+    private void HandleRotation()
+    {
+        float horizontalInput = Input.GetAxis("Horizontal"); 
+        currentAngle += horizontalInput * rotationSpeed * Time.deltaTime;
+        currentAngle %= 360;
+        UpdateCameraPosition();
+    }
+
+    private void UpdateCameraPosition()
+    {
+        
+        float radianAngle = currentAngle * Mathf.Deg2Rad;
+        float x = target.position.x + Mathf.Cos(radianAngle) * radius;
+        float z = target.position.z + Mathf.Sin(radianAngle) * radius;
+        transform.position = new Vector3(x, target.position.y + verticalOffset, z);
+        transform.LookAt(target.position);
+    }
+}
