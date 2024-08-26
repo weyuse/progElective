9b342eadd64b0cff152db39dde6956a1f8e06db3
weyuse <45233958+weyuse@users.noreply.github.com>
2024-08-26 15:50:40 +0100

    deleted lookoutcollider
    wasn't needed

--- RPS_Elective/Assets/Scripts/LookoutCollider.cs
+++
@@ -1,9 +0,0 @@
-﻿using UnityEngine;
-
-/// <summary>
-/// Attached to the sphere that acts as a sensor to 'see' other participants in the battle arena
-/// </summary>
-public class LookoutCollider : MonoBehaviour
-{
-    
-}
